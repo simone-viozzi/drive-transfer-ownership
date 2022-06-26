2c8059b7a3662aa96a7fd936468ef7616662a366
simone-viozzi <simoneviozzi97@gmail.com>
2022-06-26 14:01:08 +0200

    I already said I was sorry

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pydrive2.files import GoogleDriveFile
 import os
 import shutil
 import json
+import cachetools.func
 
 basepath = os.path.dirname(os.path.abspath(__file__))
 
@@ -202,7 +203,7 @@ class Drive:
         
 
         # to cache the files and avoid reqery the drive every time
-        self.files_cache = {}
+        #self.files_cache = {}
         
 
     def autenticate(self, name, debug=False):
@@ -246,37 +247,34 @@ class Drive:
             self.tmp.Upload()
         else:
             self.tmp = tmp_query[0]
-        
+    
 
-    # da debuggare!!
+    @cachetools.func.ttl_cache(maxsize=128, ttl=10 * 60)
+    def _raw_list_files(self, query, debug=False) -> "list[GoogleDriveFile]":
+        printd(f"doing query: {query}", debug)
+        return self.drive.ListFile({'q': query}).GetList()
+
+
     def list_files(self, path: str, folder_id='root', index=0, debug=False) -> "list[GoogleDriveFile]":
         if not path.startswith('/'):
             path = f'/{path}'
+        if not path.endswith('/'):
+            path = f'{path}/'
 
-        printd("\n" + "#" * 80, debug)
+        printd("\n" + "#" * 50, debug)
         printd(f"list_files path {path}, index {index}", debug)
-
-        if path in self.files_cache:
-            printd(f"path in cache", debug)
-            return self.files_cache[path]
-        printd(f"path not in cache", debug)
 
         folders = path.split('/')[:-1 or None]
 
-        l: "list[GoogleDriveFile]" = self.drive.ListFile({'q': f"'{folder_id}' in parents and trashed=false"}).GetList()
+        printd(f"folder_id: {folder_id}", debug)
+        l: "list[GoogleDriveFile]" = self._raw_list_files(f"'{folder_id}' in parents and trashed=false", debug)
 
         cache_path = '/'
         for folder in folders[1:index+1]:
             cache_path += f'{folder}/'
-
-        titles = map(lambda f: f['title'], l)
-           
-        printd(f"cache_path {cache_path} -> {list(titles)}", debug)
-
-        self.files_cache[cache_path] = l
         
-
         if index == len(folders) - 1:
+            printd("exit condition", debug)
             return l
 
         index += 1
@@ -287,7 +285,8 @@ class Drive:
 
         for f in l:
             if f['title'] == next_folder and f['mimeType'] == 'application/vnd.google-apps.folder':
-                return self.list_files(path, f['id'], index)
+                printd("recurse", debug)
+                return self.list_files(path, f['id'], index, debug)
         
         raise Exception(f'Folder {next_folder} not found')
 
@@ -331,7 +330,6 @@ class Drive:
         
 
 
-
 if __name__ == '__main__':
     shutil.rmtree(f'{basepath}/tmp/', ignore_errors=True)
     os.mkdir(f'{basepath}/tmp/')
@@ -339,34 +337,49 @@ if __name__ == '__main__':
     univpm1: Drive = Drive('univpm1')
 
     print("\n" + "*" * 80)
-    print(f"listing files in /tmp_folder/bla")
-    l = univpm1.list_files("/tmp_folder/bla")
+    print(f"listing files in /secondo_anno/Anno 2 Sem.2/Algebra e Logica/appunti")
 
-    l = map(lambda f: f['title'], l)
-
-    print(list(l))
-
-    print("the cache is:")
-    print(univpm1.files_cache.keys())
-
-    print("\n" + "*" * 80)
-    print(f"listing files in /tmp_folder/")
-
-    l = univpm1.list_files("/tmp_folder/")
+    l = univpm1.list_files("/secondo_anno/Anno 2 Sem.2/Algebra e Logica/appunti",  debug=True)
 
     l = map(lambda f: f['title'], l)
 
     print(list(l))
 
     print("\n" + "*" * 80)
-    print(f"listing files in /")
+    print(f"listing files in /secondo_anno/Anno 2 Sem.2/Analisi Numerica/Moodle/soluzioni su spazi piani")
 
-    l = univpm1.list_files("/")
+    l = univpm1.list_files("/secondo_anno/Anno 2 Sem.2/Analisi Numerica/moodle/soluzioni su spazi piani",  debug=True)
 
     l = map(lambda f: f['title'], l)
 
     print(list(l))
 
+
+    #print("\n" + "*" * 80)
+    #print(f"listing files in /tmp_folder/bla")
+    #l = univpm1.list_files("/tmp_folder/bla", debug=True)
+#
+    #l = map(lambda f: f['title'], l)
+#
+    #print(list(l))
+#
+    #print("\n" + "*" * 80)
+    #print(f"listing files in /tmp_folder/")
+#
+    #l = univpm1.list_files("/tmp_folder/", debug=True)
+#
+    #l = map(lambda f: f['title'], l)
+#
+    #print(list(l))
+#
+    #print("\n" + "*" * 80)
+    #print(f"listing files in /")
+#
+    #l = univpm1.list_files("/",  debug=True)
+#
+    #l = map(lambda f: f['title'], l)
+#
+    #print(list(l))
 
     #print(json.dumps(univpm1.about, indent=4))
 
